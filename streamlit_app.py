f3ccbbc90d79d049f3f8729325c3350b57262686
Sathish Panthagani <sathish39893@gmail.com>
2024-05-03 14:30:26 +0530

    model selection box

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,14 @@ from langchain.prompts import PromptTemplate
 st.title("🦜🔗 Langchain - Blog Outline Generator App")
 
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
-
+model_name = st.sidebar.selectbox("Select a model",
+    ("gpt-3.5-turbo", "gpt-3.5-turbo-0125", "gpt-3.5-turbo-1106", "text-davinci-003"),
+    index=None,
+    placeholder="please select a model")
 
 def blog_outline(topic):
     # Instantiate LLM model
-    llm = OpenAI(model_name="text-davinci-003", openai_api_key=openai_api_key)
+    llm = OpenAI(model_name=model_name, openai_api_key=openai_api_key)
     # Prompt
     template = "As an experienced data scientist and technical writer, generate an outline for a blog about {topic}."
     prompt = PromptTemplate(input_variables=["topic"], template=template)
@@ -23,7 +26,7 @@ def blog_outline(topic):
 with st.form("myform"):
     topic_text = st.text_input("Enter prompt:", "")
     submitted = st.form_submit_button("Submit")
-    if not openai_api_key:
+    if not openai_api_key and model_name:
         st.info("Please add your OpenAI API key to continue.")
     elif submitted:
         blog_outline(topic_text)
