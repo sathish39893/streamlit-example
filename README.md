5660b5d9692070565e811c233c7924a173f81222
Sathish Panthagani <sathish39893@gmail.com>
2024-05-03 15:43:19 +0530

    add streamlit inputs for chat model

--- README.md
+++ README.md
@@ -4,3 +4,40 @@ Edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
 
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
+## Run Application
+### Step 1: Activate environment
+```
+# Windows command prompt
+.venv\Scripts\activate.bat
+
+# Windows PowerShell
+.venv\Scripts\Activate.ps1
+
+# macOS and Linux
+source .venv/bin/activate
+```
+Once activated, you will see your environment name in parentheses before your prompt. "(.venv)"
+
+### Step 2: Install dependencies
+
+Run below command to install dependencies
+
+`pip install streamlit`
+
+### Step 3: Test Streamlit
+
+Test streamlit by running below command:
+
+`streamlit hello`
+
+a webpage open with hello app with default pages, please close browser and exit command prompt as well.
+
+### Step 3: Run this application
+
+Run the application with below command
+
+`streamlit run streamlit_app.py`
+
+If this doesn't work, use the long-form command:
+
+`python -m streamlit run streamlit_app.py`
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,46 @@
 import streamlit as st
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.memory import ChatMessageHistory
 
-st.title("🦜🔗 Langchain - Blog Outline Generator App")
+st.title("Blog Outline Generator App \n with 🦜🔗 Langchain - Streamlit")
 
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 model_name = st.sidebar.selectbox("Select a model",
     ("gpt-3.5-turbo", "gpt-3.5-turbo-0125", "gpt-3.5-turbo-1106", "text-davinci-003"),
     index=None,
     placeholder="please select a model")
+temperature_input = st.sidebar.slider("Select temperature", min_value=0.1,step=0.1,value=0.2,max_value=1.0)
 
 def blog_outline(topic):
     # Instantiate LLM model
-    llm = OpenAI(model_name=model_name, openai_api_key=openai_api_key)
+    chat = ChatOpenAI(model_name=model_name, openai_api_key=openai_api_key, temperature=temperature_input)
     # Prompt
-    template = "As an experienced data scientist and technical writer, generate an outline for a blog about {topic}."
-    prompt = PromptTemplate(input_variables=["topic"], template=template)
-    prompt_query = prompt.format(topic=topic)
-    # Run LLM model
-    response = llm(prompt_query)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are an experienced data scientist and technical writer, generate an outline for a blog about a topic from user input"
+            ),
+            MessagesPlaceholder(variable_name="topics")
+        ]
+    )
+    chain = prompt | chat
+
+    demo_ephemeral_chat_history = ChatMessageHistory()
+    demo_ephemeral_chat_history.add_user_message(topic)
+
+    response = chain.invoke({"topics": demo_ephemeral_chat_history.messages})
     # Print results
-    return st.info(response)
+    return st.info(response.content)
 
 
 with st.form("myform"):
     topic_text = st.text_input("Enter prompt:", "")
     submitted = st.form_submit_button("Submit")
-    if not openai_api_key and model_name:
+    if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
+    elif not model_name:
+        st.info("Please select a model to continue.")
     elif submitted:
         blog_outline(topic_text)
